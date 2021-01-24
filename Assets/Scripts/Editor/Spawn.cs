03cfbeb800aff037197eb9e59ea22b9de33ff360
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-24 17:57:54 +0200

    Added scripts to place the bricks

---
+++ Assets/Scripts/Editor/Spawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class Spawn : EditorWindow
+{
+    // Add menu item
+    [MenuItem("CONTEXT/SpawnBricks/Spawn Bricks")]
+    static void SpawnBricks(MenuCommand command)
+    {
+        SpawnBricks body = (SpawnBricks)command.context;
+        body.PlaceBricks();
+    }
+}
