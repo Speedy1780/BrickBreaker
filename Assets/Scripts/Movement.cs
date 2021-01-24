9e1c55c00574cca9f0503b3052769754eb17320d
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-24 23:34:00 +0200

    Implement base class for player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class Movement : MonoBehaviour
+{
+    protected Transform myTransform;
+
+    protected virtual void Start() => myTransform = transform;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    protected abstract void MovePlayer();
+}
