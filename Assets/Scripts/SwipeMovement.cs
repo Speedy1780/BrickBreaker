c5e38da284cff47aac4f67c5a4f99afc8be3c725
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-24 23:34:57 +0200

    Implement swipe movement

---
+++ Assets/Scripts/SwipeMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SwipeMovement : Movement
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private AnimationCurve horizontalLimit;
+    [SerializeField] private Vector3 playerPosition;
+
+    protected override void MovePlayer()
+    {
+        playerPosition.x = GetHorizontalPosition();
+        myTransform.position = playerPosition;
+    }
+
+    //Returns the x position of the player
+    float GetHorizontalPosition() => horizontalLimit.Evaluate(mainCamera.ScreenToViewportPoint(Input.mousePosition).x);
+}
