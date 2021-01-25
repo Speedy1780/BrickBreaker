88cf8561f8ed7d559c376a3d760b68ae1c9b6d66
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-25 14:43:23 +0200

    Implement movement

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 4;
+    Vector3 lastVelocity;
+    Transform myTransform;
+
+    void Start()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+
+        myTransform = transform;
+    }
+
+    private void LateUpdate() => lastVelocity = rb.velocity;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.velocity = (myTransform.position - collision.transform.position).normalized * speed;
+        }
+        else
+        {
+            Bounce(collision.GetContact(0).normal);
+
+
+            if (collision.gameObject.CompareTag("Brick"))
+                Destroy(collision.gameObject);
+        }
+    }
+
+    void Bounce(Vector3 normal)
+    {
+        rb.velocity = Vector3.Reflect(lastVelocity, normal).normalized * speed;
+
+        //Prevent ball being stuck in place
+        if (rb.velocity.magnitude == 0)
+            rb.velocity = Vector3.up * speed;
+    }
+}
