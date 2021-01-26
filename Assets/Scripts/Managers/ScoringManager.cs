c7934587a43f86ad3fea8fcf0f512430c86c5d01
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-26 10:06:16 +0200

    Implement scoring manager

---
+++ Assets/Scripts/Managers/ScoringManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringManager : Singleton<ScoringManager>
+{
+    [SerializeField] private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    void AddScore(int points)
+    {
+        score += points;
+    }
+}
