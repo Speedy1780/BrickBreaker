0d53fb6f8deb6e8c627898609bdf472ac033e753
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-26 22:23:34 +0200

    Implement UI manager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField] private Text ScoreText;
+    [SerializeField] private Text LifeText;
+
+    public void SetScore(int score) => ScoreText.text = $"Score: {score}";
+    public void SetLife(int lives) => LifeText.text = $"Life: {lives}";
+
+}
