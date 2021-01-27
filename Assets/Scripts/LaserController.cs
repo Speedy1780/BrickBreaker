fe6758e5287b23013cce7f8271c92ba7571583c4
DESKTOP-6CO3E6G\User <sandro17.18.19@gmail.com>
2021-01-27 22:09:39 +0200

    Documented laser controller

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -3,7 +3,7 @@
 public class LaserController : MonoBehaviour
 {
     private const int IgnorePhysics = 8;
-    private const int LaserLayer = 11;
+    private const int LaserLayer = 11; //Only collides with bricks and boundaries
 
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 5;
@@ -15,24 +15,19 @@ public class LaserController : MonoBehaviour
         EventManager.EGameEnded += DestroyLaser;
     }
 
-    private void OnDisable()
-    {
-        EventManager.EGameEnded -= DestroyLaser;
-    }
+    private void OnDisable() => EventManager.EGameEnded -= DestroyLaser;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        PoolManager.Instance.AddToPool(ID, gameObject);
-        rb.velocity = Vector3.zero;
-    }
+    private void OnCollisionEnter(Collision collision) => PoolManager.Instance.AddToPool(ID, gameObject);
 
     void InitializeLaser()
     {
         if (rb == null)
             rb = GetComponent<Rigidbody>();
 
-        gameObject.layer = IgnorePhysics;
         rb.velocity = Vector3.up * speed;
+
+        //Ignore all collisions for 0.2 seconds to prevent laser colliding with player and being destroyed
+        gameObject.layer = IgnorePhysics;
         Invoke(nameof(Activate), 0.2f);
     }
 
